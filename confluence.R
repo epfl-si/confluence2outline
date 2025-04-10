f9107d4e9fbfaddba26dff340e45a9eb7084012d
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-04-10 09:44:25 +0200

    [feature] Parse `<ContentProperty>` nodes
    - Turn the `//object[@class="ContentProperty"]` nodesets into tibbles
    - `props_tibble`: turn `<property>` nodes without a `class` attribute
    into like-named columns, e.g. `content_id`
    - `classful_props_tibble`: turn `<property>` nodes with a `class`
    attributes into a “qualified” column named e.g. `containerContent.Page`

--- confluence.R
+++ confluence.R
@@ -38,17 +38,18 @@ confluence_id <- function (node_or_nodeset) {
         xml_text
 }
 
-#' Format the “props” of each node in a nodeset, into a tibble
+#' Format the classless “props” of each node in a nodeset, into a tibble
 #'
 #' @param objects_nodeset An XML nodeset (*not* a list of many
 #'     nodesets)
 #' @return A tibble with as many rows as there are objects in
 #'     objects_nodeset. The columns of the returned tibble are the XML
-#'     “name” attributes of any property nodes found, and the values
-#'     are the XML texts found within said property nodes.
+#'     “name” attributes of any `property` sub-nodes that do *not*
+#'     have a `class` attribute; and the values are the XML texts
+#'     found within said property nodes.
 props_tibble <- function(objects_nodeset) {
     objects_nodeset %>%
-    xml_find_all('property', flatten=FALSE) %>%
+    xml_find_all('property[not(@class)]', flatten=FALSE) %>%
     tibble(row = seq_along(.),
            props = .) %>%
         rowwise() %>%
@@ -56,6 +57,32 @@ props_tibble <- function(objects_nodeset) {
             row = row,
             pkeys = xml_attr(props, 'name'),
             pvals = xml_text(props)) %>%
+        pivot_wider(names_from = pkeys, values_from = pvals) %>%
+        select(-row)
+}
+
+#' Format the “props” that have a `class` for each node in a nodeset, into a tibble
+#'
+#' @param objects_nodeset An XML nodeset (*not* a list of many
+#'     nodesets)
+#' @return A tibble with as many rows as there are objects in
+#'     objects_nodeset. The columns of the returned tibble are named
+#'     as `foo.Bar`, where `foo` (e.g. "content") is the `name`
+#'     attribute, and `Bar` is the `class` attribute, of any
+#'     `property` sub-nodes that have a `class` attribute; and the
+#'     values are the XML texts found within said property nodes.
+classful_props_tibble <- function(objects_nodeset) {
+    objects_nodeset %>%
+    xml_find_all('property[@class]', flatten=FALSE) %>%
+    tibble(row = seq_along(.),
+           props = .) %>%
+        rowwise() %>%
+        reframe(
+            row = row,
+            pnames = xml_attr(props, 'name'),
+            pclasses = xml_attr(props, 'class'),
+            pvals = xml_text(props)) %>%
+        mutate(pkeys=paste(pnames, pclasses, sep="."), .keep = "unused") %>%
         pivot_wider(names_from = pkeys, values_from = pvals) %>%
         select(-row)
 }
@@ -74,6 +101,7 @@ page_versions <- {
     ns <- entities_xml %>% xml_find_all('//object[@class="Page"]')
     tibble(content_id = ns %>% confluence_id) %>%
         mutate(ns %>% props_tibble) %>%
+        mutate(ns %>% classful_props_tibble) %>%
         mutate(ns %>%
                xml_find_all(c('collection[@name="contentProperties"]',
                               'element[@class="ContentProperty"]',
@@ -82,4 +110,11 @@ page_versions <- {
                             flatten = FALSE) %>%
                tibble(ids = .) %>% rowwise %>%
                transmute(content_property_ids = xml_text(ids) %>% list))
+}
+
+content_properties <- {
+    ns <- entities_xml %>% xml_find_all('//object[@class="ContentProperty"]')
+    tibble(property_id = ns %>% confluence_id) %>%
+        mutate(ns %>% props_tibble) %>%
+        mutate(ns %>% classful_props_tibble)
 }
