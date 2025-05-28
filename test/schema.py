86e76e46d2adbd6e93879fc7f548a18fc474ce69
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-05-28 13:13:05 +0200

    [testing][feature] Reverse-engineered schema for Outline JSON files
    The `genson` Python library got me started; then, ChatGPT helped me
    tie the various recursive knots within the final schema, get the
    `propertyNames.pattern`s right, and more.
    
    (When invoked from a top-level Python rather than Reticulate,)
    `test/schema.py` now always uses this schema.

--- test/schema.py
+++ test/schema.py
@@ -4,15 +4,18 @@ import sys
 import json
 import jsonschema
 
-if __name__ == "__main__" and os.getenv("R_SESSION_INITIALIZED") is None:
-    with open(sys.argv[-2]) as f:
-        data = json.load(f)
-
-    with open(sys.argv[-1]) as f:
+def validate_json_outline (json_string):
+    with open(os.path.join(os.path.dirname(__file__),
+                           "outline-schema-reverse-engineered.json")) as f:
         schema = json.load(f)
+    jsonschema.validate(instance=json.loads(json_string), schema=schema)
+
+if __name__ == "__main__" and os.getenv("R_SESSION_INITIALIZED") is None:
+    with open(sys.argv[-1]) as f:
+        data = f.read()
 
     try:
-        jsonschema.validate(instance=data, schema=schema)
+        validate_json_outline(data)
         print("✔ JSON is valid.")
     except jsonschema.ValidationError as e:
         print("❌ JSON validation error:")
