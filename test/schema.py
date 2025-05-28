f7ba60b515d91c07837897ca5b7d8fa4d65d031a
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-05-28 14:30:52 +0200

    [testing][workaround]&[fix] `test/schema.py` for Reticulate-friendliness
    - There is no `__file__` in Reticulate
    - Return True for `stopifnot()` goodness
    - Do `main.R`'s surface check through a file, as there appears to be a limit (of
      roughly 20 megabytes) to the size of strings that Reticulate can pass through

--- test/schema.py
+++ test/schema.py
@@ -1,21 +1,28 @@
+import inspect
 import os
 import sys
 
 import json
 import jsonschema
 
-def validate_json_outline (json_string):
-    with open(os.path.join(os.path.dirname(__file__),
+
+def validate_json_outline_string (json_string):
+    with open(os.path.join(os.path.dirname(inspect.stack()[-1].filename),
                            "outline-schema-reverse-engineered.json")) as f:
         schema = json.load(f)
     jsonschema.validate(instance=json.loads(json_string), schema=schema)
+    return True
+
+
+def validate_json_outline_file (path):
+    with open(path) as f:
+        data = f.read()
+    return validate_json_outline_string(data)
+
 
 if __name__ == "__main__" and os.getenv("R_SESSION_INITIALIZED") is None:
-    with open(sys.argv[-1]) as f:
-        data = f.read()
-
     try:
-        validate_json_outline(data)
+        validate_json_outline_file(sys.argv[-1])
         print("✔ JSON is valid.")
     except jsonschema.ValidationError as e:
         print("❌ JSON validation error:")
