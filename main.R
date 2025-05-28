093dde09740696484df09527f98e3c8f9a5c7875
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-05-28 14:08:43 +0200

    [testing][feature] Simple Python schema checker

--- main.R
+++ main.R
@@ -50,6 +50,14 @@ outline <- transform(archive_path, confluence)
 outline_json <- outline$meta %>%
     jsonlite::toJSON(pretty = TRUE, auto_unbox = TRUE)
 
+############################ Test more ###############################
+
+if (interactive()) {
+    source_python("test/schema.py")
+    stopifnot("outline$meta matches the Outline JSON schema" =
+                  validate_json_outline(outline_json))
+}
+
 ############################### Load #################################
 
 if ("--skip-zip" %in% cmdline) {
