51ed10ce216097f0f59f215714f50b2f8d4fd82c
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-05-30 15:05:36 +0200

    [fix]&[improve] `--small-sample`
    - Make the sample less small
    - Ensure that it is closed under the relevant “self-foreign-key” relationships i.e.
    `parent.Page` and `latest.version`
    - Keep a copy of the original data set around

--- main.R
+++ main.R
@@ -37,9 +37,36 @@ test.page <-
 test.page %>% pull(body) %>% write("test/data/services-etc.xml")
 
 if ("--small-sample" %in% cmdline) {
-    confluence$pages <- test.page
+    confluence_orig <- list(pages = confluence$pages,
+                            attachments = confluence$attachments)
+    confluence$pages <- local({
+        restricted.pages <- sample_frac(confluence$pages, 0.2)
+        if (! (test.page$page_id %in% restricted.pages$page_id)) {
+            restricted.pages <- restricted.pages %>%
+                bind_rows(test.page)
+        }
+        sans_na <- function(vec) { vec[! is.na(vec)] }
+    
+        while(! (
+            all( (restricted.pages$parent.Page %>% sans_na())
+                %in% restricted.pages$page_id )
+            &
+            all( (restricted.pages$latest.version %in% restricted.pages$page_id ) )
+        )) {
+            print(glue::glue("Completing parents and latest.versions of {
+                                  nrow(restricted.pages) } rows"))
+            restricted.pages <-
+                confluence$pages %>%
+                filter((page_id %in% c(
+                                         restricted.pages$page_id,
+                                         restricted.pages$parent.Page %>% sans_na(),
+                                         restricted.pages$latest.version
+                                         )))
+        }
+        restricted.pages
+    })
     confluence$attachments <- confluence$attachments %>%
-        filter(containerContent.Page == test.page$page_id)
+        filter(containerContent.Page %in% confluence$pages$page_id)
 }
 
 ############################ Transform ###############################
