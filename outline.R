a94610a4ec53056cc0c12fd821c7cac2da1a0902
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-05-31 19:03:42 +0200

    [fix] `collection.name` must point to the JSON file (sans extension)
    This fixes the following crash:
    
    ```
    {"error":"Invalid input for Fragment.fromJSON","level":"error","message":"ImportTask failed to persist data for bea1056b-f092-4096-a67f-60aee6478024","stack":"RangeError: Invalid input for Fragment.fromJSON
        at Function.fromJSON (/opt/outline/node_modules/prosemirror-model/dist/index.cjs:284:40)
        at Function.fromJSON (/opt/outline/node_modules/prosemirror-model/dist/index.cjs:1189:30)
        at DocumentHelper.toProsemirror (/opt/outline/build/server/models/helpers/DocumentHealper.js:43:37)
    ```
    (truncated)

--- outline.R
+++ outline.R
@@ -246,11 +246,15 @@ transform <- function (archive_path, confluence) {
 
     collection.blurb <- empty.document
 
+    meta.filename.stem <- archive_path %>%
+        base::basename() %>%
+        str_extract("^(.*?)-[0-9-]*[.]xml[.]zip", group = 1)
+
     meta <- list(
         attachments = attachments %>%
             transform.attachments.meta(),
         collection = list(
-            name = "Collection restored from Confluence®",
+            name = meta.filename.stem,
             urlId = urlIds$get(1),
                                         # XXX
             id = "077fde82-7e36-4849-b4e5-a78b9d3feb64",  # TODO: there is a
@@ -273,16 +277,11 @@ transform <- function (archive_path, confluence) {
         documents = transformed.documents %>%
             transform.document.meta())
 
-    meta.filename <- archive_path %>%
-        base::basename() %>%
-        str_extract("^(.*?)-[0-9-]*[.]xml[.]zip", group = 1) %>%
-        paste0(".json")
-
     list(.tmp = list(outline = outline, dc = dc),
          documents = transformed.documents,
          attachments = attachments,
          meta = meta,
-         meta.filename = meta.filename)
+         meta.filename = paste0(meta.filename.stem, ".json"))
 }
 
 rewrite.attachments <- function (.attachments, zip.from, zip.to) {
